df5861810004b4408b7a6f38cad96daca98dc9ce
Kidaii <mathieu.jouzeau@efrei.net>
2025-12-12 14:46:29 +0100

    Séquence 3 c
    Refactor main function to handle two cases: operations with two numbers and square of one number. Improved error handling and added return statements.

--- src/main.c
+++ src/main.c
@@ -3,33 +3,54 @@
 #include <string.h>
 #include "./../include/calculator.h"
 
-int main(int argc, char *argv[]){
-	if (argc == 4)
-	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
-	}
-	else {printf("Erreur de parametres");}
-	return 0;
-}
+int main(int argc, char *argv[]) {
 
-		
+    // cas 1 : opérations à 2 nombres
+    if (argc == 4) {
+        char* op = argv[1];
+        char* a  = argv[2];
+        char* b  = argv[3];
+        double r = 0;
+
+        if (strcmp(op, "add") == 0) {
+            r = _add(atof(a), atof(b));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "sub") == 0) {
+            r = _sub(atof(a), atof(b));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "mul") == 0) {
+            r = _mul(atof(a), atof(b));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "div") == 0) {
+            r = _div(atof(a), atof(b));
+            printf("%lf", r);
+        }
+        else {
+            printf("Erreur de parametres");
+            return 1;
+        }
+
+        return 0;
+    }
+
+    // cas 2 : carré (1 seul nombre)
+    if (argc == 3) {
+        char* op = argv[1];
+        char* a  = argv[2];
+
+        if (strcmp(op, "car") == 0) {
+            double r = _car(atof(a));
+            printf("%lf", r);
+            return 0;
+        }
+
+        printf("Erreur de parametres");
+        return 1;
+    }
+
+    printf("Erreur de parametres");
+    return 1;
+}
